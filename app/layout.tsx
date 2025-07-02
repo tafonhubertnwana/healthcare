744aeec9d036b372246869d2f9c9fa83da8205cf
Tafon Herbert <tafonsoftwarespecialist@gmail.com>
2025-07-02 04:45:57 +0100

    creatinf the success page

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const fontSans = FontSans({
 });
 
 export const metadata: Metadata = {
-  title: "CarePulse",
+  title: "Smart HealthCare",
   description:
     "A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.",
   icons: {
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-dark-300 font-sans antialiased",
+          "min-h-screen bg-dark-300 font-sans antialiased overflow-hidden",
           fontSans.variable
         )}
       >
